ae135cf40a91166f8c5c56fa7b127680e43b3401
Dell <Dell@ANANTA>
2023-09-01 10:54:13 +0530

    Add project files.

---
+++ Project4/Delegate/Calculation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4.Delegate
+{
+    public class Calculation
+    {   public delegate int MyDelegate(int x, int y);
+        public delegate double Delegate(double x, double y);
+        public int Addition(int a,int b)
+        {
+            return a + b;
+        }
+        public double Division(double a,double b) 
+        {
+            return a / b;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Calculation c1 = new Calculation();
+            MyDelegate myDelegate = c1.Addition;
+            // invoke the method using delegate//
+            int result = myDelegate.Invoke(45, 67);
+            Console.WriteLine(" The addition is "+result);
+            
+        }
+    }
+}
---
+++ Project4/Delegate/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4.Delegate
+{
+    public class User
+    {
+        public delegate string MyDelegate(string username);
+        public string AcseptName(string name)
+        {
+            string n = "Ananta";
+            Console.WriteLine(n.ToUpper());
+            return n;
+
+        }
+        static void Main(string[] args)
+        {
+            User user = new User();
+            MyDelegate myDelegate=new MyDelegate( user.AcseptName);
+            string result = myDelegate.Invoke("Ananta");
+            Console.WriteLine(result);
+        }
+        
+        
+
+    }
+}
